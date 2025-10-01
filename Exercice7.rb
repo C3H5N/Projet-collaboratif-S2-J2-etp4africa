5120c15674b9163942c823bb4b135a1e2d2080db
mirfou111 <fourmitux111@gmail.com>
2025-10-01 20:53:38 +0000

    Exercice 7 effectue

---
+++ Exercice7.rb
@@ -0,0 +1,14 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+# Le code ci-dessus demande à l'utilisateur d'entrer quelque chose, puis le stocke dans la variable 'user_name'
+# et l'affiche ensuite.
+print "===============================\n"
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+# Le code ci-dessus est similaire au précédent, mais utilise 'print' pour indiquer à l'utilisateur où taper sa réponse.
+print "===============================\n"
+
